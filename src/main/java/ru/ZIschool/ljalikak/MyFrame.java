dd8f7b07b3b11b843f0993942c9a2820e436b716
Almoom <moredoom@yandex.ru>
2021-02-26 22:38:57 +0300

    color begin

--- src/main/java/ru/ZIschool/ljalikak/MyFrame.java
+++ src/main/java/ru/ZIschool/ljalikak/MyFrame.java
@@ -9,52 +9,21 @@ class MyFrame
         implements ActionListener {
 
     // Components of the Form
+    // insert, update, delete, select
     private Container c;
     private JLabel title;
-    private JLabel name;
-    private JTextField tname;
-    private JLabel mno;
-    private JTextField tmno;
-    private JLabel gender;
-    private JRadioButton male;
-    private JRadioButton female;
-    private ButtonGroup gengp;
-    private JLabel dob;
-    private JComboBox date;
-    private JComboBox month;
-    private JComboBox year;
-    private JLabel add;
-    private JTextArea tadd;
-    private JCheckBox term;
+    private JLabel login;
+    private JTextField tlogin;
+    private JLabel password;
+    private JTextField tpassword;
+    private JLabel race;
+    private JRadioButton human;
+    private JRadioButton mutant;
+    private JRadioButton ghoul;
+    private ButtonGroup racep;
     private JButton sub;
     private JButton reset;
-    private JTextArea tout;
-    private JLabel res;
-    private JTextArea resadd;
 
-    private String dates[]
-            = { "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "10",
-            "11", "12", "13", "14", "15",
-            "16", "17", "18", "19", "20",
-            "21", "22", "23", "24", "25",
-            "26", "27", "28", "29", "30",
-            "31" };
-    private String months[]
-            = { "Jan", "feb", "Mar", "Apr",
-            "May", "Jun", "July", "Aug",
-            "Sup", "Oct", "Nov", "Dec" };
-    private String years[]
-            = { "1995", "1996", "1997", "1998",
-            "1999", "2000", "2001", "2002",
-            "2003", "2004", "2005", "2006",
-            "2007", "2008", "2009", "2010",
-            "2011", "2012", "2013", "2014",
-            "2015", "2016", "2017", "2018",
-            "2019" };
-
-    // constructor, to initialize the components
-    // with default values.
     public MyFrame()
     {
         setTitle("Registration Form");
@@ -62,141 +31,109 @@ class MyFrame
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
 
+
         c = getContentPane();
         c.setLayout(null);
+        c.setBackground(Color.decode("#212121"));
 
-        title = new JLabel("Registration Form");
-        title.setFont(new Font("Arial", Font.PLAIN, 30));
-        title.setSize(300, 30);
+        title = new JLabel("welcome to the radioactive forest");
+        title.setFont(new Font("Helvetica", Font.PLAIN, 30));
+        title.setForeground(new Color(20, 240, 116));
+        title.setSize(500, 30);
         title.setLocation(300, 30);
         c.add(title);
 
-        name = new JLabel("Name");
-        name.setFont(new Font("Arial", Font.PLAIN, 20));
-        name.setSize(100, 20);
-        name.setLocation(100, 100);
-        c.add(name);
+        login = new JLabel("login");
+        login.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        login.setForeground(new Color(20, 240, 116));
+        login.setSize(100, 20);
+        login.setLocation(100, 100);
+        c.add(login);
 
-        tname = new JTextField();
-        tname.setFont(new Font("Arial", Font.PLAIN, 15));
-        tname.setSize(190, 20);
-        tname.setLocation(200, 100);
-        c.add(tname);
+        tlogin = new JTextField();
+        tlogin.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        tlogin.setSize(190, 20);
+        tlogin.setLocation(200, 100);
+        c.add(tlogin);
 
-        mno = new JLabel("Mobile");
-        mno.setFont(new Font("Arial", Font.PLAIN, 20));
-        mno.setSize(100, 20);
-        mno.setLocation(100, 150);
-        c.add(mno);
+        password = new JLabel("password");
+        password.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        password.setForeground(new Color(20, 240, 116));
+        password.setSize(100, 20);
+        password.setLocation(100, 150);
+        c.add(password);
 
-        tmno = new JTextField();
-        tmno.setFont(new Font("Arial", Font.PLAIN, 15));
-        tmno.setSize(150, 20);
-        tmno.setLocation(200, 150);
-        c.add(tmno);
+        tpassword = new JTextField();
+        tpassword.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        tpassword.setSize(150, 20);
+        tpassword.setLocation(200, 150);
+        c.add(tpassword);
 
-        gender = new JLabel("Gender");
-        gender.setFont(new Font("Arial", Font.PLAIN, 20));
-        gender.setSize(100, 20);
-        gender.setLocation(100, 200);
-        c.add(gender);
+        race = new JLabel("race");
+        race.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        race.setForeground(new Color(20, 240, 116));
+        race.setSize(100, 20);
+        race.setLocation(100, 200);
+        c.add(race);
 
-        male = new JRadioButton("Male");
-        male.setFont(new Font("Arial", Font.PLAIN, 15));
-        male.setSelected(true);
-        male.setSize(75, 20);
-        male.setLocation(200, 200);
-        c.add(male);
+        human = new JRadioButton("human");
+        human.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        human.setForeground(new Color(20, 240, 116));
+        human.setSelected(true);
+        human.setSize(100, 20);
+        human.setLocation(200, 200);
+        c.add(human);
 
-        female = new JRadioButton("Female");
-        female.setFont(new Font("Arial", Font.PLAIN, 15));
-        female.setSelected(false);
-        female.setSize(80, 20);
-        female.setLocation(275, 200);
-        c.add(female);
+        mutant = new JRadioButton("mutant");
+        mutant.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        mutant.setForeground(new Color(20, 240, 116));
+        mutant.setSelected(false);
+        mutant.setSize(100, 20);
+        mutant.setLocation(300, 200);
+        c.add(mutant);
 
-        gengp = new ButtonGroup();
-        gengp.add(male);
-        gengp.add(female);
+        ghoul = new JRadioButton("ghoul");
+        ghoul.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        ghoul.setForeground(new Color(20, 240, 116));
+        ghoul.setSelected(false);
+        ghoul.setSize(100, 20);
+        ghoul.setLocation(400, 200);
+        c.add(ghoul);
 
-        dob = new JLabel("DOB");
-        dob.setFont(new Font("Arial", Font.PLAIN, 20));
-        dob.setSize(100, 20);
-        dob.setLocation(100, 250);
-        c.add(dob);
+        racep = new ButtonGroup();
+        racep.add(human);
+        racep.add(ghoul);
+        racep.add(mutant);
 
-        date = new JComboBox(dates);
-        date.setFont(new Font("Arial", Font.PLAIN, 15));
-        date.setSize(50, 20);
-        date.setLocation(200, 250);
-        c.add(date);
-
-        month = new JComboBox(months);
-        month.setFont(new Font("Arial", Font.PLAIN, 15));
-        month.setSize(60, 20);
-        month.setLocation(250, 250);
-        c.add(month);
-
-        year = new JComboBox(years);
-        year.setFont(new Font("Arial", Font.PLAIN, 15));
-        year.setSize(60, 20);
-        year.setLocation(320, 250);
-        c.add(year);
-
-        add = new JLabel("Address");
-        add.setFont(new Font("Arial", Font.PLAIN, 20));
-        add.setSize(100, 20);
-        add.setLocation(100, 300);
-        c.add(add);
-
-        tadd = new JTextArea();
-        tadd.setFont(new Font("Arial", Font.PLAIN, 15));
-        tadd.setSize(200, 75);
-        tadd.setLocation(200, 300);
-        tadd.setLineWrap(true);
-        c.add(tadd);
-
-        term = new JCheckBox("Accept Terms And Conditions.");
-        term.setFont(new Font("Arial", Font.PLAIN, 15));
-        term.setSize(250, 20);
-        term.setLocation(150, 400);
-        c.add(term);
-
-        sub = new JButton("Submit");
-        sub.setFont(new Font("Arial", Font.PLAIN, 15));
-        sub.setSize(100, 20);
-        sub.setLocation(150, 450);
+        sub = new JButton("new");
+        sub.setForeground(Color.decode("#212121"));
+        sub.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        sub.setSize(200, 30);
+        sub.setLocation(90, 250);
         sub.addActionListener(this);
         c.add(sub);
 
-        reset = new JButton("Reset");
-        reset.setFont(new Font("Arial", Font.PLAIN, 15));
-        reset.setSize(100, 20);
-        reset.setLocation(270, 450);
+        reset = new JButton("continue")
+        {
+            @Override
+            public boolean isBackgroundSet() {
+                return super.isBackgroundSet();
+            }
+        };
+        reset.setForeground(Color.decode("#ff"));
+        reset.setBackground(Color.decode("#ff0000"));
+//        reset.setContentAreaFilled(false);
+        reset.setOpaque(true);
+        reset.setBorderPainted(false);
+//        reset.setOpaque();
+
+        reset.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        reset.setSize(200, 30);
+        reset.setLocation(300, 250);
         reset.addActionListener(this);
         c.add(reset);
 
-        tout = new JTextArea();
-        tout.setFont(new Font("Arial", Font.PLAIN, 15));
-        tout.setSize(300, 400);
-        tout.setLocation(500, 100);
-        tout.setLineWrap(true);
-        tout.setEditable(false);
-        c.add(tout);
-
-        res = new JLabel("");
-        res.setFont(new Font("Arial", Font.PLAIN, 20));
-        res.setSize(500, 25);
-        res.setLocation(100, 500);
-        c.add(res);
-
-        resadd = new JTextArea();
-        resadd.setFont(new Font("Arial", Font.PLAIN, 15));
-        resadd.setSize(200, 75);
-        resadd.setLocation(580, 175);
-        resadd.setLineWrap(true);
-        c.add(resadd);
-
+        repaint();
         setVisible(true);
     }
 
@@ -205,52 +142,37 @@ class MyFrame
     // by the user and act accordingly
     public void actionPerformed(ActionEvent e)
     {
-        if (e.getSource() == sub) {
-            if (term.isSelected()) {
-                String data1;
-                String data
-                        = "Name : "
-                        + tname.getText() + "\n"
-                        + "Mobile : "
-                        + tmno.getText() + "\n";
-                if (male.isSelected())
-                    data1 = "Gender : Male"
-                            + "\n";
-                else
-                    data1 = "Gender : Female"
-                            + "\n";
-                String data2
-                        = "DOB : "
-                        + (String)date.getSelectedItem()
-                        + "/" + (String)month.getSelectedItem()
-                        + "/" + (String)year.getSelectedItem()
-                        + "\n";
-
-                String data3 = "Address : " + tadd.getText();
-                tout.setText(data + data1 + data2 + data3);
-                tout.setEditable(false);
-                res.setText("Registration Successfully..");
-            }
-            else {
-                tout.setText("");
-                resadd.setText("");
-                res.setText("Please accept the"
-                        + " terms & conditions..");
-            }
-        }
-
-        else if (e.getSource() == reset) {
-            String def = "";
-            tname.setText(def);
-            tadd.setText(def);
-            tmno.setText(def);
-            res.setText(def);
-            tout.setText(def);
-            term.setSelected(false);
-            date.setSelectedIndex(0);
-            month.setSelectedIndex(0);
-            year.setSelectedIndex(0);
-            resadd.setText(def);
-        }
+//        if (e.getSource() == sub) {
+//            if (term.isSelected()) {
+//                String data1;
+//                String data
+//                        = "Name : "
+//                        + tlogin.getText() + "\n"
+//                        + "Mobile : "
+//                        + tpassword.getText() + "\n";
+//                if (human.isSelected())
+//                    data1 = "Gender : Male"
+//                            + "\n";
+//                else
+//                    data1 = "Gender : Female"
+//                            + "\n";
+//                ;
+//                res.setText("Registration Successfully..");
+//            }
+//            else {
+//                resadd.setText("");
+//                res.setText("Please accept the"
+//                        + " terms & conditions..");
+//            }
+//        }
+//
+//        else if (e.getSource() == reset) {
+//            String def = "";
+//            tlogin.setText(def);
+//            tpassword.setText(def);
+//            res.setText(def);
+//            term.setSelected(false);
+//            resadd.setText(def);
+//        }
     }
 }
